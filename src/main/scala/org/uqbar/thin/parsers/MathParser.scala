f428c23a8d109aa01fce582765f097db962b3eb6
FernandoPetryszyn <fer_jjp@hotmail.com>
2015-03-06 23:24:37 -0300

    Planteo basico del modelo, sujeto a cambios

--- src/main/scala/org/uqbar/thin/parsers/MathParser.scala
+++ src/main/scala/org/uqbar/thin/parsers/MathParser.scala
@@ -3,9 +3,29 @@ package org.uqbar.thin.parsers
 import scala.util.parsing.combinator._
 
 object MathParser extends MathParser
+
 trait MathParser extends RegexParsers {
-	def apply(input: String) = parseAll(???, input) match {
-		case Success(result, _) => result
-		case NoSuccess(msg, _) => throw ParseException(msg)
-	}
+  trait Exp
+  
+  case class N(value: Int) extends Exp
+  case class Add(l: Exp, r: Exp) extends Exp
+  case class Sub(l: Exp, r: Exp) extends Exp
+  case class Div(l: Exp, r: Exp) extends Exp
+  case class Mul(l: Exp, r: Exp) extends Exp
+  
+  
+  def exp: Parser[Exp] = number | add
+  def factor: Parser[Exp] = number | "(" ~> exp <~ ")"
+  def term: Parser[Exp] = div | mul | number
+  def number = "-?[0-9]+".r ^^ { n => N(n.toInt) }
+  def add = term ~ ("+" ~ exp).? ~ factor ^^ { case l ~ _ ~ r => Add(l, r) }
+  def sub = term ~ ("-" ~ exp).? ~ factor ^^ { case l ~ _ ~ r => Sub(l, r) }
+  def div = factor ~ "/" ~ term ^^ { case l ~ _ ~ r => Div(l, r) }
+  def mul = factor ~ "*" ~ term ^^ { case l ~ _ ~ r => Mul(l, r) }
+  
+  
+  def apply(input: String) = parseAll(???, input) match {
+    case Success(result, _) => result
+    case NoSuccess(msg, _)  => throw ParseException(msg)
+  }
 }
--- src/main/scala/org/uqbar/thin/parsers/XMLParser.scala
+++ src/main/scala/org/uqbar/thin/parsers/XMLParser.scala
@@ -4,8 +4,21 @@ import scala.util.parsing.combinator._
 
 object XMLParser extends XMLParser
 trait XMLParser extends RegexParsers {
+  
+  //No se como representar a las tags que no tienen cuerpo enteramente, asi que por ahora es simplemente un string vacio (en vez de otra clase)
+  //Es facilmente cambiable
+  trait Element
+  //Representa algo que tiene hijos
+  case class Node(id: String,attrb: List[Attribute],body: String,children: List[Element]) extends Element
+  //Representa las hojas del arbol
+  case class Leaf(id: String,attrb: List[Attribute],body: String) extends Element
+  
+  case class Attribute(id: String,value:String)
+  
 	def apply(input: String) = parseAll(???, input) match {
 		case Success(result, _) => result
 		case NoSuccess(msg, _) => throw ParseException(msg)
 	}
+  
+  
 }
