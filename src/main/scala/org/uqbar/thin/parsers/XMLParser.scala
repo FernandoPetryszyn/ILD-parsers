3a85fc5f989e223b3ab0b602a16d84de18acaf68
FernandoPetryszyn <fer_jjp@hotmail.com>
2015-03-06 23:25:26 -0300

    Remove useless comments

--- src/main/scala/org/uqbar/thin/parsers/XMLParser.scala
+++ src/main/scala/org/uqbar/thin/parsers/XMLParser.scala
@@ -8,9 +8,7 @@ trait XMLParser extends RegexParsers {
   //No se como representar a las tags que no tienen cuerpo enteramente, asi que por ahora es simplemente un string vacio (en vez de otra clase)
   //Es facilmente cambiable
   trait Element
-  //Representa algo que tiene hijos
   case class Node(id: String,attrb: List[Attribute],body: String,children: List[Element]) extends Element
-  //Representa las hojas del arbol
   case class Leaf(id: String,attrb: List[Attribute],body: String) extends Element
   
   case class Attribute(id: String,value:String)
